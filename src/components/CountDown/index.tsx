49410e6920cc4cfcf4953eb1f7fe43cae91da495
Thiago Lima <tlima7601@gmail.com>
2026-02-02 20:44:09 -0300

    CountDown feito

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -2,8 +2,8 @@ import Styles from "./styles.module.css"
 
 export const Countdown = () => {
     return (
-        <div>
-            
+        <div className={Styles.countdown}>
+            00:00
         </div>
     )
 }
