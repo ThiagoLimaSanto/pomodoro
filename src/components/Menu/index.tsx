f370210cabf0c36d48ec8c6412f353504bc9a990
Thiago Lima <tlima7601@gmail.com>
2026-02-02 22:45:17 -0300

    feito a funcao para mudar o tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,49 @@
-import { FaHome, FaSun, FaHistory, FaCog } from "react-icons/fa";
+import { FaHome, FaHistory, FaCog } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
+import { FiSun } from "react-icons/fi";
 import Styles from "./styles.module.css"
+import { useState, useEffect } from "react";
+
+type AvailableThemes = 'dark' | 'light'
 
 export const Menu = () => {
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark'
+
+        return storageTheme
+    });
+
+    const nextThemeIcon = {
+        dark: <FiSun />,
+        light: <MdDarkMode />
+    }
+
+    const handleThemeChange = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault();
+
+        setTheme(prevTheme => {
+            return prevTheme === 'dark' ? 'light' : 'dark'
+        })
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme])
+
     return (
         <nav className={Styles.menu}>
-            <a className={Styles.menuLink} href="#">
+            <a className={Styles.menuLink} href="#" aria-label='Ir para a home' title="Home">
                 <FaHome />
             </a>
-            <a className={Styles.menuLink} href="#">
+            <a className={Styles.menuLink} href="#" aria-label='Ver histórico' title="Histórico">
                 <FaHistory />
             </a>
-            <a className={Styles.menuLink} href="#">
+            <a className={Styles.menuLink} href="#" aria-label='Ir para Configurações' title="Configurações">
                 <FaCog />
             </a>
-            <a className={Styles.menuLink} href="#">
-                <FaSun />
+            <a onClick={handleThemeChange} className={Styles.menuLink} href="#" aria-label='Mudar Tema' title="Mudar Tema">
+                {nextThemeIcon[theme]}
             </a>
         </nav>
     )
